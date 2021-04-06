eb4b5492fb8ad9a4ffae6f0f29262673e214059c
MaskMaster4048 <logan104plumlee@gmail.com>
2021-04-06 04:19:03 -0500

    Wrote everything

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -28,7 +28,14 @@ import edu.wpi.first.cameraserver.*;
 import edu.wpi.first.networktables.*;
 
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Core;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.AraryList;
 
 
 /**
@@ -58,10 +65,18 @@ public class Robot extends TimedRobot {
 
   public Mat sourceImg = new Mat(); //input from whatever the camera says
   public Mat outputImg = new Mat(); //same thing but blurry and HSV
+  public Mat workingImg = new Mat(); //for vision processing output
 
   public boolean isRedPath;
   public int currBall;
   public double timerValue;
+  public Timer autoTimer;
+
+  public int intakeActivationPosition; //TODO this needs to be found
+  public Scalar low = new Scalar(25, 50, 50); //TODO this is an estimation
+  public Scalar high = new Scalar(35, 255, 255);//TODO estimation
+  public double minBallArea = 1;
+  public double minMarkerArea = 1;
 
   @Override
   public void robotInit() { //TODO what are the "m_"?
@@ -108,6 +123,7 @@ public class Robot extends TimedRobot {
           continue;
         }
         Imgproc.cvtColor(sourceImg, outputImg, Imgproc.COLOR_BGR2HSV);
+        Core.inRange(outputImg, low, high, outputImg);
         outputStream.putFrame(outputImg);
       }
     }).start();
@@ -172,9 +188,115 @@ public class Robot extends TimedRobot {
 }
   @Override
   public void autonomousInit() {
-
+    autoTimer = new Timer();
+    autoTimer.start();
+    m_drive.tankDrive(0.5, 0.5);
+    currball = 0;
   }
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    Point ballPos;
+    if (currBall != 3) ballPos = findTarget();
+    else ballPos = findEnd();
+    if (tryPickUpBall(ballPos)){
+      if (currBall == 0){
+        double time = autoTimer.get();
+        if (time < 2) isRedPath = true;
+        else isRedPath = false;
+      } else if (currBall == 1){
+        if (isRedPath){
+          m_drive.tankDrive(0.5, 0.25);
+          Timer.delay(1);
+        } else {
+          m_drive.tankDrive(0.1, 1);
+          Time.delay(1);
+        }
+      } else if(currBall == 2){
+        if (isRedPath){
+          m_drive.tankDrive(0.1, 1);
+          Time.delay(1);
+        } else {
+          m_drive.tankDrive(0.5, 0.25);
+          Timer.delay(1);
+        }
+      } else {
+        if (isRedPath) {
+          m_drive.tankDrive(-0.25, -.5);
+          Timer.delay(1);
+        } else {
+          m_drive.tankDrive(-0.5, -0.25);
+          Timer.delay(1);
+        }
+      }
+    }
+    if (ballPos == null) m_drive.tankDrive(0, 0); //safety
+    fullSpeed = 0.5;
+    turnBias = 0.5;
+    if (ballPos.x < 640/2){
+      float missing = (float)ballPos.x/640*2*turnBias;
+      m_drive.tankDrive(fullSpeed-missing, fullSpeed);
+    } else {
+      float missing = (float)(640-ballPos.x)/640*2*turnBias;
+      m_drive.tankDrive(fullSpeed, fullSpeed-missing);
+    }
+  }
 
+  public boolean tryPickUpBall(Point ballPos){
+    while (ballPos.y >= intakeActivationPosition){
+      intake.setSpeed(1);
+      m_drive.tankDrive(0.5, 0.5);
+      Timer.delay(1);
+      ballPos = findTarget();
+    }
+    intake.setSpeed(0);
+  }
+
+  public Point findEnd(){
+    List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+    List<Point> points = new ArrayList<Point>();
+    Imgproc.findContours(outputImg, contours, workingImg, Imgproc.RETR_EXTERNAL,
+      Imgproc.CHAIN_APPROX_SIMPLE);
+    for (int i = 0; i < contours.size(); i++) {
+      double contourArea = Imgproc.contourArea(contours.get(i));
+      if (contourArea < minMarkerArea) continue;
+      Rect boundRect = Imgproc.boundingRect(contours.get(i));
+      float ratio = (float)boundRect.width/boundRect.height;
+      if (ratio > 0.90) continue;
+      float solidity = (float)Imgproc.contourArea(contours.get(i))
+        /(boundRect.width*boundRect.height);
+      if (solidity < 0.8) continue;
+      double centerX = boundRect.x + (boundRect.width / 2);
+      double centerY = boundRect.y + (boundRect.height / 2);
+      points.add(new Point(centerX, centerY));
+    }
+    size = points.size();
+    if (size > 2) System.err.println("too many targets to find the" +
+      "the end");
+    int sumx = 0;
+    int sumy = 0;
+    for (i=0; i<size; i++){
+      sumx += points.get(i).x;
+      sumy += points.get(i).y;
+    }
+    return new Point(sumx/size, sumy/size);
+  }
+
+  public Point findTarget(){
+    List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+    Imgproc.findContours(outputImg, contours, workingImg, Imgproc.RETR_EXTERNAL,
+      Imgproc.CHAIN_APPROX_SIMPLE);
+    for (int i = 0; i < contours.size(); i++) {
+      double contourArea = Imgproc.contourArea(contours.get(i));
+      if (contourArea < minBallArea) continue;
+      Rect boundRect = Imgproc.boundingRect(contours.get(i));
+      float ratio = (float)boundRect.width/boundRect.height;
+      if (ratio < 0.90 || ratio > 1.10) continue;
+      float solidity = (float)Imgproc.contourArea(contours.get(i))
+        /(boundRect.width*boundRect.height);
+      if (solidity < 0.7 || solidity > 0.9) continue;
+      double centerX = boundRect.x + (boundRect.width / 2);
+      double centerY = boundRect.y + (boundRect.height / 2);
+      return new Point(centerX, centerY);
+    }
+  }
 }
